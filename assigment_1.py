b7ffa2099bacbab1540705af88cb0db6729999ed
kavya-venu <kavyasrivenutharla@gmail.com>
2024-07-19 18:44:55 +0530

    day1
    day1

---
+++ assigment_1.py
@@ -0,0 +1,6 @@
+# write a program to find area of a circle
+#area=pi*r*r
+r=float(input("enter the radius of the circle"))
+pi=3.14
+area=pi*r*r
+print("area of circle:",area) 
---
+++ assigment_2.py
@@ -0,0 +1,7 @@
+# write a program to find perimeter of a circle
+#c=2*pi*r
+per=float(input("enter the perimeter of a circle:"))
+r=3
+c=2*3.14*r
+print("perimeter of a circle:",c)
+
